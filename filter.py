b25233a6eaa0f9bf205e624a41ac714dd340d72d
jey <jey@jeys-MacBook-Pro.local>
2019-11-15 18:01:21 +0300

    Get maximum and minimum count of numbers in a given dataset

---
+++ filter.py
@@ -0,0 +1,46 @@
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+from operator import add
+
+conf = SparkConf().setAppName("Find maximum and minimum occurring values in pyspark")
+sc = SparkContext(conf=conf)
+
+# Read file into RDD
+lines = sc.textFile("dataset.txt")
+
+# Call collect() to get all data
+llist = lines.collect()
+digit_to_count = {}
+for i in llist:
+    data = sc.parallelize(list(i))
+    counts = data.map(lambda x:
+                      (x, 1)).reduceByKey(add).sortBy(lambda x: x[1],
+                                                      ascending=False).collect()
+
+    for (word, count) in counts:
+        digit_to_count[word] = count
+        print("{}: {}".format(word, count))
+
+print(digit_to_count)
+
+'''Calculation of the mean of all the values counted in the reducer'''
+count = 0
+_sum = 0
+for key in digit_to_count:
+    count += 1
+    _sum += digit_to_count[key]
+
+
+# maximum digit value
+print('Maximum digit value ', {max(item for item in digit_to_count.values())})
+
+# minimum digit value
+print('Minimum digit value ', {min(item for item in digit_to_count.values())})
+
+
+print(count, _sum)
+# if including the whitespaces and period in the data
+print('The mean is: ', (_sum/count))
+# if not in cluding the period and whitespaces in the data
+print('The mean is: ', (_sum-100)/(count-2))
